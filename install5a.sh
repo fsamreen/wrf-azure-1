19635fb084013166031e3882a598d7af4be7c068
fsamreen <aziaf2002@yahoo.com>
2017-09-11 13:16:58 +0100

    Create install5a.sh

---
+++ install5a.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/wrflibrary/LIBRARIES
+sudo tar xzvf netcdf-4.1.3.tar.gz     #or just .tar if no .gz present
+cd netcdf-4.1.3
+./configure --prefix=$DIR/netcdf --disable-dap --disable-netcdf-4 --disable-shared
+sudo apt-get install make -y
+make
+make install
+cd ..
