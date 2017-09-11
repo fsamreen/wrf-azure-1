98cbc0a512ead5e818706c67fabd67f35e6a5745
fsamreen <aziaf2002@yahoo.com>
2017-09-11 13:23:57 +0100

    Update install5a.sh

--- install5a.sh
+++ install5a.sh
@@ -2,8 +2,8 @@
 cd /home/wrflibrary/LIBRARIES
 sudo tar xzvf netcdf-4.1.3.tar.gz     #or just .tar if no .gz present
 cd netcdf-4.1.3
-./configure --prefix=$DIR/netcdf --disable-dap --disable-netcdf-4 --disable-shared
+sudo ./configure --prefix=$DIR/netcdf --disable-dap --disable-netcdf-4 --disable-shared
 sudo apt-get install make -y
-make
-make install
+sudo make
+sudo make install
 cd ..
