3060cd5a427655fca7a7654323fcf40f68481244
fsamreen <aziaf2002@yahoo.com>
2017-09-11 12:47:40 +0100

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 #cd Build_WRF
 # put at end of installation-test code
-cd ~/TESTS
-wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/Fortran_C_tests.tar
-tar -xf Fortran_C_tests.tar
+cd /wrfstorage/wrfinstall1/TESTS
+sudo wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/Fortran_C_tests.tar
+sudo tar -xf Fortran_C_tests.tar
 #Test #1: Fixed Format Fortran Test: TEST_1_fortran_only_fixed.f
-gfortran TEST_1_fortran_only_fixed.f
+sudo gfortran TEST_1_fortran_only_fixed.f
 ./a.out
 #The following should print out to the screen: SUCCESS test 1 fortran only fixed format
 #Test #2: Free Format Fortran: TEST_2_fortran_only_free.f90
-gfortran TEST_2_fortran_only_free.f90
+sudo gfortran TEST_2_fortran_only_free.f90
 ./a.out
 #The following should print out to the screen:#Assume Fortran 2003: has FLUSH, ALLOCATABLE, derived type, and ISO C Binding SUCCESS test 2 fortran only free format
 #Test #3: C: TEST_3_c_only.c
-gcc TEST_3_c_only.c
+sudo gcc TEST_3_c_only.c
 ./a.out
 #The following should print out to the screen: SUCCESS test 3 c only
 # Test #4: Fortran Calling a C Function (our gcc and gfortran have different defaults, so we force both to always use 64 bit [-m64] when combining them): TEST_4_fortran+c_c.c, and TEST_4_fortran+x_f.f90
-gcc -c -m64 TEST_4_fortran+c_c.c
-gfortran -c -m64 TEST_4_fortran+c_f.f90
-gfortran -m64 TEST_4_fortran+c_f.o TEST_4_fortran+c_c.o
+sudo gcc -c -m64 TEST_4_fortran+c_c.c
+sudo gfortran -c -m64 TEST_4_fortran+c_f.f90
+sudo gfortran -m64 TEST_4_fortran+c_f.o TEST_4_fortran+c_c.o
 ./a.out
