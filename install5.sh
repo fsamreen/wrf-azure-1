b53e315ba6830fb32addd5840efc15d89c6145f7
fsamreen <aziaf2002@yahoo.com>
2017-09-11 12:58:41 +0100

    Update install5.sh

--- install5.sh
+++ install5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ~/Build_WRF/LIBRARIES
-tar xzvf netcdf-4.1.3.tar.gz     #or just .tar if no .gz present
+cd wrfstorage/wrfinstall1/Build_WRF/LIBRARIES
+sudo tar xzvf netcdf-4.1.3.tar.gz     #or just .tar if no .gz present
 cd netcdf-4.1.3
 ./configure --prefix=$DIR/netcdf --disable-dap --disable-netcdf-4 --disable-shared
 sudo apt-get install make -y
