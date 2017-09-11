d1432da6c611ad20b443bb3a77fb37024bca1628
fsamreen <aziaf2002@yahoo.com>
2017-09-11 13:11:57 +0100

    Create install3a.sh

---
+++ install3a.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /wrflibraries/LIBRARIES
+sudo mkdir LIBRARIES
+cd LIBRARIES
+sudo wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/mpich-3.0.4.tar.gz
+sudo wget http://archive.ubuntu.com/ubuntu/pool/universe/m/mpich/mpich_3.2.orig.tar.gz
+sudo wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/netcdf-4.1.3.tar.gz
+sudo wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/jasper-1.900.1.tar.gz
+sudo wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/libpng-1.2.50.tar.gz
+sudo wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/zlib-1.2.7.tar.gz
