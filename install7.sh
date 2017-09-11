766fdddd146cfccd8ef3d7843431d4818a79e5b5
fsamreen <aziaf2002@yahoo.com>
2017-09-11 13:36:43 +0100

    Update install7.sh

--- install7.sh
+++ install7.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ~/Build_WRF/LIBRARIES
-tar xzvf mpich-3.0.4.tar.gz
+cd /home/wrflibrary/LIBRARIES
+sudo tar xzvf mpich-3.0.4.tar.gz
 cd mpich-3.0.4
-./configure --prefix=$DIR/mpich
-make
-make install
+sudo ./configure --prefix=$DIR/mpich
+sudo make
+sudo make install
